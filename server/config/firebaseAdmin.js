8e4529eb1339d41d57c20f834cc7f22ec21ce09d
Amit929 <amitkumarsuthar01@gmail.com>
2026-01-09 18:11:27 +0530

    Remove service account file and initialize Firebase Admin using env variables for Railway

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -1,23 +1,42 @@
+// import admin from "firebase-admin";
+// import fs from "fs";
+// import path from "path";
+// import { fileURLToPath } from "url";
+
+// // Fix __dirname in ES module
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// // Read service account JSON
+// const serviceAccount = JSON.parse(
+//   fs.readFileSync(
+//     path.join(__dirname, "../firebase-service-account.json"),
+//     "utf-8"
+//   )
+// );
+
+// // Initialize Firebase Admin
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
+
+// export default admin;
+
 import admin from "firebase-admin";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
+import dotenv from "dotenv";
 
-// Fix __dirname in ES module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+dotenv.config();
 
-// Read service account JSON
-const serviceAccount = JSON.parse(
-  fs.readFileSync(
-    path.join(__dirname, "../firebase-service-account.json"),
-    "utf-8"
-  )
-);
-
-// Initialize Firebase Admin
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
+    databaseURL: "https://stackoverflo-54055-default-rtdb.firebaseio.com",
+  });
+}
 
 export default admin;
+
