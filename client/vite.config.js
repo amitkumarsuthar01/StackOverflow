f197b0dab944434a7a0802c62c533783fec9827f
Amit929 <amitkumarsuthar01@gmail.com>
2026-01-10 09:33:59 +0530

    Vite config file updated for production

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,28 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   test: {
-//     environment: "jsdom",
-//     globals: true,
-//     setupFiles: "./src/setupTests.js",
-//   },
-//   server: {
-//     open: true,
-//     host: true,
-//     proxy: {
-//       "/api": {
-//         // target: "http://localhost:3000",
-//         target: "stackoverflow-production-d987.up.railway.app",
-//         changeOrigin: true,
-//         secure: false,
-//       },
-//     }
-//   }
-// })
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
